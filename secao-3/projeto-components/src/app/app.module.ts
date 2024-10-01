0b0d1547a66ce0142b03179468d3a7bb2360a24e
Marvin Thomaz do Nascimento <marvin.db1@senac.br>
2024-10-01 09:58:18 -0300

    Seção 3: criação de componente de card

--- secao-3/projeto-components/src/app/app.module.ts
+++ secao-3/projeto-components/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 
@@ -11,8 +10,7 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
