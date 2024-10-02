d3df4cfc296fefb3ec3bc5b72e23e998066424a1
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-02 21:29:38 +0000

    ng if

--- secao-4/ng_if/src/app/app.component.ts
+++ secao-4/ng_if/src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng_if';
+  minhaProp = true;
+  persona: any = {
+    status: 1,
+    name: undefined,
+  }
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy() {
+    // return {};
+    // return [];
+    // return 1;
+    // return "teste";
+    // return "0";
+    // return '0';
+    // return "false";
+    // return this.persona.status;
+  }
+
+  isFalsy() {
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.persona.name;
+    // return this.persona.address?.street;
+  }
 }
