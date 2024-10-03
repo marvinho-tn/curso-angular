9f9d9e1513019584eaf61d26d0e2796e9bb16bcc
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-03 13:53:50 +0000

    utilização do ngStyle

--- secao-4/ng_style/src/app/app.component.ts
+++ secao-4/ng_style/src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng_style';
+  fontSize: number = 15;
+  textColor: 'white' | 'orange' = 'white';
+  buttonText: 'Branco' | 'Laranja' = 'Laranja';
+  styleString: string = 'font-size: 15px; color: white;';
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+  }
+  
+  increaseFontSize() {
+    this.fontSize += this.fontSize / 10;
+
+    this._updateStaticStyles();
+  }
+  
+  decreaseFontSize() {
+    this.fontSize -= this.fontSize / 10;
+
+    this._updateStaticStyles();
+  }
+
+  resetStyle() {
+    this.fontSize = 15;
+    this.textColor = 'white';
+    this.buttonText = 'Laranja';
+
+    this._updateStaticStyles();
+  }
+
+  toggleFontColor() {
+    if(this.textColor === 'white') {
+      this.textColor = 'orange';
+      this.buttonText = 'Branco';
+    } else {
+      this.textColor = 'white';
+      this.buttonText = 'Laranja';
+    }
+
+    this._updateStaticStyles();
+  }
+
+  private _updateStaticStyles() {
+    this._updateStyleString();
+    this._updateStyleObj();
+  }
+
+  private _updateStyleString() {
+    this.styleString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+  }
+
+  private _updateStyleObj() {
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor,
+    };
+  }
 }
