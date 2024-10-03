ec6a25fcb8f704f52feb581c90e7887c199b4b39
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-03 19:31:10 +0000

    component user-details dinamico

--- secao-5/users-filter-project/src/app/components/user-details/user-details.component.ts
+++ secao-5/users-filter-project/src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from '../../interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent {
-
+  @Input({ required: true })
+  user: IUser = { } as IUser;
 }
