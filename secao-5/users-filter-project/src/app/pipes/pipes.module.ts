0d2bb64672139ff6f22f5eedb36ecfd5e6cb0b8d
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-03 20:18:04 +0000

    pipe de data nativo

--- secao-5/users-filter-project/src/app/pipes/pipes.module.ts
+++ secao-5/users-filter-project/src/app/pipes/pipes.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
 import { StatusPipe } from './status.pipe';
-import { DatePipe } from './date.pipe';
 
 @NgModule({
     declarations: [
         PhonePipe,
         AddressPipe,
         StatusPipe,
-        DatePipe,
     ],
     exports: [
         PhonePipe,
         AddressPipe,
         StatusPipe,
-        DatePipe,
     ],
 })
 export class PipesModule { }
