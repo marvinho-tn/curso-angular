b7e70a302cef8f7f1230dbad388189891100298f
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-04 18:07:13 +0000

    criação dos filtros de nome, status e data

--- secao-5/users-filter-project/src/app/app.component.ts
+++ secao-5/users-filter-project/src/app/app.component.ts
@@ -12,11 +12,12 @@ export class AppComponent implements OnInit {
   userSelected: IUser = { } as IUser;
   showUserDetails: boolean = false;
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   
   ngOnInit() {
     // Suposta chamada HTTP
     setTimeout(() => {
-      this.usersList = UsersList;
+      this.usersListFiltered = this.usersList = UsersList;
     }, 100);
   }
 
@@ -26,6 +27,57 @@ export class AppComponent implements OnInit {
   }
 
   onFilter(filterOptions: IFilterOptions) {
-    console.log(filterOptions);
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
+    filteredList = this.filterUserListByStatus(filterOptions.status, filteredList);
+
+    return filteredList;
+  }
+
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+    
+    if(NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user: IUser) => user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+
+    return filteredList;
+  }
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, usersList: IUser[]): IUser[] {
+    const START_DATE_TYPPED = startDate !== undefined;
+    const END_DATE_TYPPED = endDate !== undefined;
+    
+    let filteredList: IUser[] = usersList;
+
+    if(START_DATE_TYPPED) {
+      filteredList = filteredList.filter((user) => new Date(user.dataCadastro) >= startDate);
+    }
+
+    if(END_DATE_TYPPED) {
+      filteredList = filteredList.filter((user) => new Date(user.dataCadastro) <= endDate);
+    }
+
+    return filteredList;
+  }
+
+  filterUserListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_TYPPED = status === undefined;
+
+    if(STATUS_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user: IUser) => user.ativo === status);
+
+    return filteredList;
   }
 }
