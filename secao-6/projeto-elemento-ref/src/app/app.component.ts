7d2a27947d9423c5e4a7efa4be3ba2ed778524f0
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-07 21:21:46 +0000

    Element ref através de injeção de dependencia

--- secao-6/projeto-elemento-ref/src/app/app.component.ts
+++ secao-6/projeto-elemento-ref/src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   @ViewChild('minhaDiv')
   divElement!: ElementRef<HTMLDivElement>;
   
-  ngAfterViewInit(): void {
+  constructor(private readonly _elRef: ElementRef<HTMLElement>) { }
+
+  ngOnInit() {
+    console.log(this._elRef);
+  }
+
+  ngAfterViewInit() {
     this.divElement.nativeElement.style.backgroundColor = 'orange';
     this.divElement.nativeElement.textContent = 'Minha div orange';
     this.divElement.nativeElement.classList.add('minha-classe');
+
+    const minhaDiv = this._elRef.nativeElement.querySelector("#minha-outra-div") as HTMLDivElement;
+
+    minhaDiv.style.backgroundColor = 'blue';
+    minhaDiv.textContent = 'Minha outra div encontrada!';
+    minhaDiv.classList.add('minha-classe');
   }
 }
