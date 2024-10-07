c9e95571c20f6a0d0b4d1e50a1b1ce71d7dc8823
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-10-07 21:03:16 +0000

    elementRef em componentes

--- secao-6/projeto-elemento-ref/src/app/app.component.ts
+++ secao-6/projeto-elemento-ref/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-elemento-ref';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('minhaDiv')
+  divElement!: ElementRef<HTMLDivElement>;
+  
+  ngAfterViewInit(): void {
+    this.divElement.nativeElement.style.backgroundColor = 'orange';
+    this.divElement.nativeElement.textContent = 'Minha div orange';
+    this.divElement.nativeElement.classList.add('minha-classe');
+  }
 }
--- secao-6/projeto-elemento-ref/src/app/app.module.ts
+++ secao-6/projeto-elemento-ref/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +8,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
